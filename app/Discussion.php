6d964d7ec281203fc400933f3d01e2b0da3d6781
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-03 15:18:47 +0300

    Some Editings

---
+++ app/Discussion.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Channel;
+use App\Reply;
+
+class Discussion extends Model
+{
+    protected $fillable = ["title", "content", "user_id", "channel_id", "slug"];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  purge: [
-    './resources/views/**/*.blade.php',
-    './resources/css/**/*.css',
-  ],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/custom-forms')
-  ]
-}
+    theme: {
+        extend: {
+            colors: {
+                mainColor: "#89C9C9",
+                btnBlueColor: "#0662FF"
+            },
+            fontFamily: {
+                sans: ["Ubuntu", "Montserrat"]
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
